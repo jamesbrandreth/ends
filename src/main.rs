420a7f89d6f26cfb9f76ee381f70c3964a45ca55
James Brandreth <james.brandreth@gmail.com>
2024-10-23 12:00:56 +0100

    prep for DB

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,8 @@ async fn main() {
     static CLIENT_DIR: Dir<'static> = include_dir!("$CARGO_MANIFEST_DIR/src/ui/build");
     let ui = ServeDir::new(&CLIENT_DIR);
 
-    // build our application with a route
     let app = Router::new().nest_service("/", ui);
 
-    // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
---
+++ src/scripts/initialise-db.sh
@@ -0,0 +1,1 @@
+initdb data/db
